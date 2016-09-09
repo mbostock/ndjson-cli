50111dd647da3ed4a18aad91f6ffa8adfe60701e
Mike Bostock <mbostock@gmail.com>
2016-09-09 10:25:15 -0700

    Fix #11. Exit cleanly on EPIPE.

--- output.js
+++ output.js
@@ -1,3 +1,9 @@
+process.stdout.on("error", function(error) {
+  if (error.code === "EPIPE" || error.errno === "EPIPE") {
+    process.exit(0);
+  }
+});
+
 module.exports = function(value) {
   process.stdout.write(value == null ? "null\n" : JSON.stringify(value) + "\n");
 };
